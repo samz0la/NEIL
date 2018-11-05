ca71b9dd4f09fd16cee7c4782f4b676396e217e7
samuelandazola <szola3@icloud.com>
2018-11-05 10:45:10 -0700

    added enemy and collision

---
+++ app/src/main/java/samuelandazola/com/neilservices/Enemy.java
@@ -0,0 +1,57 @@
+package samuelandazola.com.neilservices;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.media.MediaMetadataRetriever.BitmapParams;
+import java.util.Random;
+
+public class Enemy extends GameObject {
+
+  private int score;
+  private int speed;
+  private Random random = new Random();
+  private Animation animation = new Animation();
+  private Bitmap spritesheet;
+
+
+  public Enemy(Bitmap res, int x, int y, int w, int h, int s, int numFrames){
+    super.x = x;
+    super.y = y;
+    width = w;
+    height = h;
+    score = s;
+
+    //the enemy has a higher chance of being faster as the game goes on and the score increases (the base plus a random number)
+    speed = 7 + (int) (random.nextDouble()*score/30);
+
+    //cap the enemy speed
+    if(speed>40)speed = 40;
+
+    Bitmap[] image = new Bitmap[numFrames];
+
+    spritesheet = res;
+
+//loops through enemy image
+    for(int i = 0; i<image.length; i++){
+      image[i] = Bitmap.createBitmap(spritesheet, 0, i*height, width, height);
+    }
+//sends the array to the animation class
+    animation.setFrames(image);
+    animation.setDelay(100-speed);
+  }
+  public void update(){
+  x-=speed;
+  animation.update();
+  }
+  public void draw(Canvas canvas){
+
+    try{
+      canvas.drawBitmap(animation.getImage(), x, y, null);
+    }catch (Exception e){}
+  }
+  @Override
+  public int getWidth(){
+    //offset slightly for more realistic collision detection
+    return width-10;
+  }
+}
--- app/src/main/java/samuelandazola/com/neilservices/GamePanel.java
+++ app/src/main/java/samuelandazola/com/neilservices/GamePanel.java
@@ -3,11 +3,12 @@ package samuelandazola.com.neilservices;
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.media.SoundPool;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
@@ -16,14 +17,18 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
   public static final int HEIGHT = 1081;
   public static final int MOVESPEED = -5;
   private long smokeStartTimer;
+  private long enemyStartTime;
   private MainThread thread;
   private Background bg;
   private Player player;
   private ArrayList<Smokepuff> smoke;
+  private ArrayList<Enemy> enemy;
+  private Random random = new Random();
 
 //automatically called when the object is called
   public GamePanel(Context context) {
     super(context);
+
 //add the callback to the surfaceholder to intercept events
     getHolder().addCallback(this);
 
@@ -57,9 +62,10 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
   public void surfaceCreated(SurfaceHolder holder) {
     bg = new Background(BitmapFactory.decodeResource(getResources(), R.drawable.background2));
     player = new Player(BitmapFactory.decodeResource(getResources(), R.drawable.neil_character2), 171, 151, 1);
+    enemy = new ArrayList<>();
     smoke = new ArrayList<>();
-
     smokeStartTimer = System.nanoTime();
+    enemyStartTime = System.nanoTime();
 
     //we can safely start the game loop
     thread.setRunning(true);
@@ -88,13 +94,57 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     if (player.getPlaying()) {
       bg.update();
       player.update();
-//time elapsed from now and when smokeStartTime started
+
+      //add enemy on timer
+      long enemyElapsed = (System.nanoTime()-enemyStartTime)/1000000;
+      if (enemyElapsed > (6000 - player.getScore()/4)){
+
+        //first enemy always goes down the middle
+        if (enemy.size()==0){
+
+          enemy.add(new Enemy(BitmapFactory.decodeResource(getResources(),
+              R.drawable.enemy1), WIDTH + 10, HEIGHT/2, 281, 161, player.getScore(), 1));
+
+        }else{
+
+          //random enemy position
+          enemy.add(new Enemy(BitmapFactory.decodeResource(getResources(), R.drawable.enemy1),
+              WIDTH+10, (int) (random.nextDouble() * (HEIGHT)), 281, 161, player.getScore(), 1));
+          enemy.add(new Enemy(BitmapFactory.decodeResource(getResources(), R.drawable.enemy2),
+              WIDTH+10, (int) (random.nextDouble() * (HEIGHT)), 281, 161, player.getScore(), 1));
+          enemy.add(new Enemy(BitmapFactory.decodeResource(getResources(), R.drawable.enemy3),
+              WIDTH+10, (int) (random.nextDouble() * (HEIGHT)), 201, 121, player.getScore(), 1));
+        }
+
+        //resets the timer
+        enemyStartTime = System.nanoTime();
+      }
+      //loops through every enemy and check collision and remove
+      for (int i = 0; i<enemy.size();i++){
+
+        //update enemy
+        enemy.get(i).update();
+
+        if (collision(enemy.get(i),player)){
+          enemy.remove(i);
+          player.setPlaying(false);
+          break;
+        }
+        //removes enemy if it is way off the screen
+        if (enemy.get(i).getX()<-100){
+          enemy.remove(i);
+          break;
+        }
+      }
+
+      //time elapsed from now and when smokeStartTime started
       long elapsed = (System.nanoTime() - smokeStartTimer)/1000000;
       if(elapsed > 120){
         smoke.add(new Smokepuff(player.getX()+50, player.getY()+150));
         smokeStartTimer = System.nanoTime();
       }
-//going to itterated through every smokepuff object in the array list smoke
+
+      //going to itterated through every smokepuff object in the array list smoke
       for (int i = 0; i<smoke.size(); i++){
         smoke.get(i).update(); //going to update every smoke object
         if(smoke.get(i).getX()<-10){
@@ -103,20 +153,36 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
       }
     }
   }
+  public boolean collision(GameObject a, GameObject b){
+    if (Rect.intersects(a.getRectangle(), b.getRectangle())){
+      return true;
+    }
+    return false;
+  }
+
   @Override
   public void draw(Canvas canvas) {
     //TODO fix screen scroll
     //super.draw(canvas);
-    //scales to entire phone scren
+
+    //scales to entire phone screen
     final float scaleFactorX = getWidth() / (WIDTH * 1.f);
     final float scaleFactorY = getHeight() / (HEIGHT * 1.f);
+
+
     if (canvas != null) {
       final int savedState = canvas.save();
       canvas.scale(scaleFactorX, scaleFactorY);
       bg.draw(canvas);
       player.draw(canvas);
+
+      //draws smoke
       for(Smokepuff sp: smoke){ //sp equals every value in the smoke array and when it itterates through each one it will draw the smoke
         sp.draw(canvas);
+      }
+      //draws enemy
+      for(Enemy e: enemy){
+        e.draw(canvas);
       }
 
       canvas.restoreToCount(savedState);

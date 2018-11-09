bc1ff859686cdc2a79c7ba240eff8a5008de85a3
samuelandazola <szola3@icloud.com>
2018-11-09 12:34:53 -0700

    added landing screen activity

--- app/src/main/java/samuelandazola/com/neilservices/controller/MainActivity.java
+++ app/src/main/java/samuelandazola/com/neilservices/controller/MainActivity.java
@@ -1,12 +1,22 @@
 package samuelandazola.com.neilservices.controller;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
+import com.hitomi.cmlibrary.OnMenuStatusChangeListener;
+import samuelandazola.com.neilservices.Game;
 import samuelandazola.com.neilservices.R;
 import samuelandazola.com.neilservices.model.db.GameDatabase;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
 
   private GameDatabase database;
 
@@ -14,6 +24,43 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_game);
+
+
+    CircleMenu circleMenu = (CircleMenu)findViewById(R.id.circle_menu);
+        circleMenu.setMainMenu(Color.parseColor("#CDCDCD"), R.mipmap.takeoff_launcher, R.mipmap.exit_launcher)
+        .addSubMenu(Color.parseColor("#CDCDCD"), R.mipmap.facebook)
+            .addSubMenu(Color.parseColor("#CDCDCD"), R.mipmap.takeoff_launcher)
+            .addSubMenu(Color.parseColor("#CDCDCD"), R.mipmap.google)
+
+        .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+
+          @Override
+          public void onMenuSelected(int index) {
+            switch (index){
+              case 0:
+                break;
+              case 1:
+                openGame();
+                break;
+              case 2:
+                break;
+            }
+          }
+
+        }).setOnMenuStatusChangeListener(new OnMenuStatusChangeListener() {
+
+      @Override
+      public void onMenuOpened() {}
+
+      @Override
+      public void onMenuClosed() {}
+
+    });
+  }
+
+  public void openGame(){
+    Intent intent = new Intent(this, Game.class);
+    startActivity(intent);
   }
 
   @Override
